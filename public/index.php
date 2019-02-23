72ac7d1bb26b3c6fc885555f6383d8deb1b78ae2
Jose Asuncion Chim Tzuc <josechimsdc1992@gmail.com>
2019-02-23 06:53:47 -0800

    hola mundo

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
-hola phalcon
+<?php
+try {
+
+	//autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    //dependency injection
+    $di = new \Phalcon\DI\FactoryDefault();
+    $di->set('view', function(){
+        $view = new \Phalcon\Mvc\View();
+        $view->setViewsDir('../app/views/');
+        return $view;
+    });
+
+    //deployment app
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
+
+} catch(\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
